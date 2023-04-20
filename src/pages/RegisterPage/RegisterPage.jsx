5e779d7acf7af8fbeee5291d9ae453bbf93cac7f
Maral-IT <amaryllis.l78@gmail.com>
2023-04-20 17:45:16 +0600

    Form validation and error handling

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,64 +1,85 @@
-import React, { useState } from 'react';
+import React from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import styles from './registerpage.module.css';
 import { authServices } from '../../services/auth';
+import { useFormik } from 'formik';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import * as yup from 'yup';
 
 const RegisterPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
+  const validationSchema = yup.object().shape({
+    email: yup
+      .string()
+      .required('This field is required')
+      .email('Enter a valid email'),
+    username: yup.string().required('This field is required'),
+    password: yup.string().required('This field is required'),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      username: '',
+    },
+    validationSchema,
+    onSubmit: async (values) => {
+      try {
+        const { data } = await registerUser(values);
+        console.log(data._doc);
+        toast('Пользователь зарегистрирован');
+      } catch (err) {
+        toast('Email или Имя уже существуют');
+      }
+    },
+  });
 
   const { registerUser } = authServices();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const newUser = {
-      username: name,
-      email,
-      password,
-    };
-    try {
-      const { data } = await registerUser(newUser);
-      console.log(data._doc);
-    } catch (err) {
-      console.log(err.response.data)
-    }
-  };
-  
   return (
     <section className={styles.wrapper}>
       <h1 className={styles.title}>Registration</h1>
-      <form className={styles.form} onSubmit={handleSubmit}>
+      <form className={styles.form} onSubmit={formik.handleSubmit}>
         <TextField
+          error={formik.errors.email}
+          helperText={formik.errors.email ? formik.errors.email : ''}
+          onBlur={formik.handleBlur}
           label='Your email'
           variant='filled'
           type='email'
+          name='email'
           fullWidth
-          required
           style={{ marginBottom: '20px' }}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          value={formik.values.email}
+          onChange={formik.handleChange}
         />
         <TextField
           label='Your name'
           variant='filled'
           type='text'
+          name='username'
+          error={formik.errors.username}
+          helperText={formik.errors.username ? formik.errors.username : ''}
+          onBlur={formik.handleBlur}
           fullWidth
-          required
           style={{ marginBottom: '20px' }}
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          value={formik.values.username}
+          onChange={formik.handleChange}
         />
         <TextField
           label='Password'
           variant='filled'
           type='password'
+          name='password'
+          error={formik.errors.password}
+          helperText={formik.errors.password ? formik.errors.password : ''}
+          onBlur={formik.handleBlur}
           fullWidth
-          required
           style={{ marginBottom: '40px' }}
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          value={formik.values.password}
+          onChange={formik.handleChange}
         />
         <Button
           fullWidth
@@ -68,6 +89,7 @@ const RegisterPage = () => {
           Register
         </Button>
       </form>
+      <ToastContainer />
     </section>
   );
 };
